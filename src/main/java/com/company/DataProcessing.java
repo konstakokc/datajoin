1ad76c2fe616de4d562309a05cac417d4795a8be
konstakokc <knitnatsnoks@gmail.com>
2019-06-24 17:09:20 +0300

    DataProcessing -> joinEvents implemented

--- src/main/java/com/company/DataProcessing.java
+++ src/main/java/com/company/DataProcessing.java
@@ -1,6 +1,15 @@
 package com.company;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.SignStyle;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
+
+import static java.time.temporal.ChronoField.*;
 
 public class DataProcessing {
     public static List<Map<String, String>> fillMissingFromSource(List<Map<String, String>> source, List<Map<String, String>> destination) {
@@ -33,5 +42,60 @@ public class DataProcessing {
             }
         }
         return "NULL";
+    }
+
+    public static List<Map<String, String>> joinEvents(List<Map<String, String>> listOfMaps) {
+        List<Map<String, String>> joinedList = new ArrayList<>();
+
+        for (int i = 0; i < listOfMaps.size(); i++) {
+            Map<String, String> currentElement = new LinkedHashMap<>(listOfMaps.get(i));
+
+            if (i + 1 == listOfMaps.size()) {
+                joinedList.add(listOfMaps.get(i));
+            } else {
+                while (isSingleEvent(currentElement, listOfMaps.get(i + 1))) {
+                    for (String key : currentElement.keySet()) {
+                        currentElement.merge(key, listOfMaps.get(i + 1).get(key), (oldVal, newVal) -> oldVal.isEmpty() ? newVal : oldVal);
+                    }
+                    i++;
+                    if (i + 1 == listOfMaps.size()) {
+                        break;
+                    }
+                }
+                joinedList.add(currentElement);
+            }
+        }
+        return joinedList;
+    }
+
+    private static boolean isSingleEvent(Map<String, String> firstReading, Map<String, String> secondReading) {
+        //        (timeСheck + idCheck ) => join
+        //        Zone/device-Check ? License-expiration Check?
+        return timeCheck(firstReading, secondReading, 300) && idCheck(firstReading, secondReading);
+    }
+
+    private static boolean timeCheck(Map<String, String> firstReading, Map<String, String> secondReading, int eventDurationInSeconds) {
+        return Math.abs(ChronoUnit.SECONDS.between(getTimeFromMap(firstReading), getTimeFromMap(secondReading)))  <= eventDurationInSeconds;
+    }
+
+    private static LocalDateTime getTimeFromMap(Map<String, String> map) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        DateTimeFormatter timeFormatter = new DateTimeFormatterBuilder()
+                .appendValue(HOUR_OF_DAY, 1, 2, SignStyle.NEVER)
+                .appendLiteral(":")
+                .appendValue(MINUTE_OF_HOUR, 2)
+                .optionalStart()
+                .appendLiteral(":")
+                .appendValue(SECOND_OF_MINUTE, 2)
+                .toFormatter();
+
+        if (!map.get("Date").isEmpty() && !map.get("Time").isEmpty()) {
+            return LocalDateTime.of(LocalDate.parse(map.get("Date"), dateFormatter), LocalTime.parse(map.get("Time"), timeFormatter));
+        } else
+            return LocalDateTime.of(LocalDate.parse(map.get("Date_gen"), dateFormatter), LocalTime.parse(map.get("Gen_time"), timeFormatter));
+    }
+
+    private static boolean idCheck(Map<String, String> firstReading, Map<String, String> secondReading) {
+        return (firstReading.get("Number").equals(secondReading.get("Number")) && firstReading.get("Family").equals(secondReading.get("Family")));
     }
 }
