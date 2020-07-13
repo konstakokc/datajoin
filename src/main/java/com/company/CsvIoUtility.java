a663fb3e18028a92a60eac12b1225d39263e278f
konstakokc <knitnatsnoks@gmail.com>
2020-07-13 16:52:49 +0300

    File restructuring

---
+++ src/main/java/com/company/CsvIoUtility.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class CsvIoUtility {
+    public static List<Map<String, String>> readCsvFile(File file) throws IOException {
+        List<Map<String, String>> listOfMappedValues = new LinkedList<>();
+        CsvMapper mapper = new CsvMapper();
+        CsvSchema schema = CsvSchema.emptySchema().withHeader().withColumnSeparator(';');
+        MappingIterator<Map<String, String>> iterator = mapper.readerFor(LinkedHashMap.class)
+                .with(schema)
+                .readValues(file);
+        while (iterator.hasNext()) {
+            listOfMappedValues.add(iterator.next());
+        }
+        return listOfMappedValues;
+    }
+
+    public static void writeCsvFile(List<Map<String, String>> listOfMappedValues, String fileName) throws IOException {
+        CsvMapper mapper = new CsvMapper();
+        PrintWriter printWriter = new PrintWriter(new FileWriter(fileName));
+
+        CsvSchema schema = new CsvSchema.Builder()
+                .addColumns(listOfMappedValues.get(0).keySet(), CsvSchema.ColumnType.STRING)
+                .build()
+                .withColumnSeparator(';')
+                .withoutQuoteChar()
+                .withHeader();
+
+        printWriter.print(mapper.writerFor(List.class).with(schema).writeValueAsString(listOfMappedValues));
+        printWriter.close();
+    }
+}
